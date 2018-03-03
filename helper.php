ad34c66d8d4026ca961176fece4e80f389b06fae
Thomas Hunziker <werbemails@bakual.ch>
2018-03-03 18:52:40 +0100

    Initial Commit

---
+++ helper.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * helper.php
+ * Copyright (C) 2011-2012 www.comunidadjoomla.org. All rights reserved.
+ * GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+jimport('joomla.filesystem.file');
+jimport('joomla.filesystem.folder');
+jimport('joomla.filesystem.path');
+abstract class modTtm_diff_tool_otfHelper
+{
+	public static function get_Diff_Info(&$params)
+	{
+		require_once (JPATH_ROOT.'/'.'modules'.'/'.'mod_ttm_diff_tool_otf'.'/'.'libraries'.'/'.'mod_ttm_diff_tool_otf_functions.php');
+		$diff_info =array();
+		$diff_info = get_diff_from_selected_packages ($params);
+
+		return $diff_info;
+	}
+}
---
+++ mod_ttm_diff_tool_otf.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * mod_ttm_diff_tool.php
+ * Copyright (C) 2011 www.comunidadjoomla.org. All rights reserved.
+ * GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+
+require_once dirname(__FILE__).'/helper.php';
+
+$diff_info = modTtm_diff_tool_otfHelper::get_Diff_Info($params);
+
+$moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
+
+require JModuleHelper::getLayoutPath('mod_ttm_diff_tool_otf', $params->get('layout', 'default'));
